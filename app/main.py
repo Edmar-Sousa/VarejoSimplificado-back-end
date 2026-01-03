c9490ce79fbafbc5daae0c4f7de2133df86073fd
Edmar-Sousa <edmarsousamendes4@gmail.com>
2026-01-03 20:36:11 -0300

    refact: change login and register logic to user repository

--- app/main.py
+++ app/main.py
@@ -1,63 +1,23 @@
-import os
-from datetime import datetime, timedelta, timezone
-
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from jose import jwt, JWTError
-
-from .database import Base, SessionLocal, engine
-from .schemas.auth import LoginRequest, RegisterRequest
-from .models.user import Users
-
-
+from fastapi import FastAPI, Depends
 from pwdlib import PasswordHash
+from sqlalchemy.orm import Session
 
-Base.metadata.create_all(bind=engine)
+from .database import get_db
+from .schemas.auth import LoginRequest, RegisterRequest
+from .repositories.user import UserRepository
+
+
 app = FastAPI()
 
-
-def get_db():
-    db = SessionLocal()
-
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-SECRET_KEY = os.getenv('SECRET_KEY')
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
-
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-
-    now = datetime.now(timezone.utc)
-    expire = now + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
-    
-    to_encode.update({"iat": now, "nbf": now, "exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    
-    return encoded_jwt
 
 
 @app.post('/login')
 def login(request: LoginRequest, db: Session = Depends(get_db)):
-
-    user = db.query(Users).where(Users.email == request.email).first()
-
-    if not user:
-        raise HTTPException(status_code=404, detail='Usuário com o email fornecido não foi encontrado.')
-    
-    password_hasher = PasswordHash.recommended()
-
-    if not password_hasher.verify(request.password, user.hashed_password):
-        raise HTTPException(status_code=401, detail='Senha esta incorreta.')
-
+    user_repo = UserRepository(db, PasswordHash.recommended())
+    user = user_repo.login(request)
 
     return {
-        'access_token': create_access_token(
+        'access_token': user_repo.create_access_token(
             data={
                 "sub": str(user.id),
                 "username": user.username,
@@ -71,20 +31,8 @@ def login(request: LoginRequest, db: Session = Depends(get_db)):
 
 @app.post('/register')
 def register(request: RegisterRequest, db: Session = Depends(get_db)):
-    password_hasher = PasswordHash.recommended()
-
-    db_user = Users(
-        username=request.username,
-        full_name=request.full_name,
-        email=request.email,
-        hashed_password=password_hasher.hash(request.password),
-        is_active=True,
-        role='admin'
-    )
-
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+    user_repo = UserRepository(db, PasswordHash.recommended())
+    db_user = user_repo.register(request)
 
     return {
         'username': db_user.username,
---
+++ app/repositories/user.py
@@ -0,0 +1,67 @@
+import os
+
+from jose import jwt
+from fastapi import HTTPException
+from pwdlib import PasswordHash
+from datetime import datetime, timedelta, timezone
+
+from sqlalchemy.orm import Session
+
+
+from app.schemas.auth import LoginRequest, RegisterRequest
+from app.models.user import Users
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+
+
+class UserRepository:
+    
+    def __init__(self, db_session: Session, password_hasher: PasswordHash):
+        self.db_session = db_session
+        self.password_hasher = password_hasher
+
+
+    def create_access_token(self, data: dict, expires_delta: timedelta | None = None):
+        to_encode = data.copy()
+
+        now = datetime.now(timezone.utc)
+        expire = now + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+        
+        to_encode.update({"iat": now, "nbf": now, "exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        
+        return encoded_jwt
+
+
+    def login(self, user_credentials: LoginRequest):
+        user = self.db_session.query(Users).where(Users.email == user_credentials.email).first()
+
+        if not user:
+            raise HTTPException(status_code=404, detail='Usuário com o email fornecido não foi encontrado.')
+
+        if not self.password_hasher.verify(user_credentials.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail='Senha esta incorreta.')
+        
+        return user
+
+
+    def register(self, user_data: RegisterRequest):
+
+        db_user = Users(
+            username=user_data.username,
+            full_name=user_data.full_name,
+            email=user_data.email,
+            hashed_password=self.password_hasher.hash(user_data.password),
+            is_active=True,
+            role='admin'
+        )
+
+        self.db_session.add(db_user)
+        self.db_session.commit()
+        self.db_session.refresh(db_user)
+
+        return db_user
