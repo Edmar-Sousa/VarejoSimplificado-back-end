55ee02492f384776806c9b078b5119d8a6043d7b
Edmar-Sousa <edmarsousamendes4@gmail.com>
2026-01-03 11:51:46 -0300

    feat: add products categories and create a router to login

--- app/main.py
+++ app/main.py
@@ -1,12 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 
-from .database import Base, engine, SessionLocal
+from sqlalchemy.orm import Session
 
+from .database import Base, SessionLocal, engine
+from .schemas.auth import LoginRequest
+from .models.user import Users
+
+
+from pwdlib import PasswordHash
+
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-@app.get('users')
-def read_users():
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-    return {"message": "List of users"}
+@app.post('/login')
+def login(request: LoginRequest, db: Session = Depends(get_db)):
+
+    user = db.query(Users).where(Users.email == request.email).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail='Usuário com o email fornecido não foi encontrado.')
+    
+    password_hasher = PasswordHash.recommended()
+
+    if not password_hasher.verify(request.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail='Senha esta incorreta.')
+
+
+    return {
+        'user': request.email,
+        'password': request.password
+    }
 
