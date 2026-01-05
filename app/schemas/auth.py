ebeac0e6cf253de00b0395ce6bb05ca78983bbc3
Edmar-Sousa <edmarsousamendes4@gmail.com>
2026-01-05 17:52:22 -0300

    feat: validate data with pydantic

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,22 +1,32 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, SecretStr, Field, validator
 
 
 class LoginRequest(BaseModel):
-    email: str
-    password: str
+    email: EmailStr = Field(..., example="Email válido")
+    password: SecretStr = Field(..., example="Senha segura")
+
+    @validator('password', pre=True)
+    def validate_password(cls, value):
+        if len(value) < 8:
+            raise ValueError('Senha deve ter pelo menos 8 caracteres.')
+
+        return value
+
 
 class LoginResponse(BaseModel):
-    access_token: str
-    token_type: str
+    access_token: str = Field(..., example="Token jwt")
+    token_type: str = Field(..., example="Bearer")
+
 
 class RegisterRequest(BaseModel):
-    username: str
-    full_name: str
-    email: str
-    password: str
+    username: str = Field(..., example="Nome de Usuario")
+    full_name: str = Field(..., example="Nome Completo")
+    email: EmailStr = Field(..., example="Email válido")
+    password: SecretStr = Field(..., example="Senha segura")
+
 
 class RegisterResponse(BaseModel):
-    username: str
-    full_name: str
-    email: str
-    is_active: bool
+    username: str = Field(..., example="Nome de Usuario")
+    full_name: str = Field(..., example="Nome Completo")
+    email: EmailStr = Field(..., example="Email válido")
+    is_active: bool = Field(..., example=True)
