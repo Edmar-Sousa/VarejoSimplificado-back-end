5c0a704fbe9cefe27c5b40983972c464f61b9798
Edmar-Sousa <edmarsousamendes4@gmail.com>
2026-01-05 17:38:20 -0300

    feat: use pydentic to response

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from pwdlib import PasswordHash
 from sqlalchemy.orm import Session
 
 from .database import get_db
-from .schemas.auth import LoginRequest, RegisterRequest
+from .schemas.auth import LoginRequest, LoginResponse, RegisterRequest, RegisterResponse
 from .schemas.products import ProductCategorySchema
 from .repositories.user import UserRepository
 from .repositories.products import ProductCategoryRepository
@@ -14,7 +14,7 @@ from .middleware.auth import is_admin, is_auth
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
@@ -30,30 +30,31 @@ def login(request: LoginRequest, db: Session = Depends(get_db)):
     user_repo = UserRepository(db, PasswordHash.recommended())
     user = user_repo.login(request)
 
-    return {
-        'access_token': user_repo.create_access_token(
-            data={
-                "sub": str(user.id),
-                "username": user.username,
-                "email": user.email,
-                "role": user.role
-            }
-        ),
-        'token_type': 'Bearer'
-    }
+    access_token = user_repo.create_access_token(
+        data={
+            "sub": str(user.id),
+            "username": user.username,
+            "email": user.email,
+            "role": user.role
+        }
+    )
 
+    return LoginResponse(
+        access_token=access_token,
+        token_type='Bearer'
+    )
 
 @app.post('/register')
 def register(request: RegisterRequest, db: Session = Depends(get_db), _=Depends(is_admin)):
     user_repo = UserRepository(db, PasswordHash.recommended())
     db_user = user_repo.register(request)
 
-    return {
-        'username': db_user.username,
-        'full_name': db_user.full_name,
-        'email': db_user.email,
-        'is_active': db_user.is_active,
-    }
+    return RegisterResponse(
+        username=db_user.username,
+        full_name=db_user.full_name,
+        email=db_user.email,
+        is_active=db_user.is_active,
+    )
 
 
 @app.post('/product/categories')
--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -5,8 +5,18 @@ class LoginRequest(BaseModel):
     email: str
     password: str
 
+class LoginResponse(BaseModel):
+    access_token: str
+    token_type: str
+
 class RegisterRequest(BaseModel):
     username: str
     full_name: str
     email: str
     password: str
+
+class RegisterResponse(BaseModel):
+    username: str
+    full_name: str
+    email: str
+    is_active: bool
